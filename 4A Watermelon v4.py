ba746ee2271c5db9d6f799019f09f5cc25f56db7
victorpc64 <perezcastillovictor@gmail.com>
2025-11-04 13:01:19 -0800

    Update 4A Watermelon v4.py

--- 4A Watermelon v4.py
+++ 4A Watermelon v4.py
@@ -1,3 +1,6 @@
+#This version tests if w is even and bigger than 2.
+#In both version starts from 4 because if it's 0 there's nothing to divide, 1 and 3 are odd and 2 there's no way to split it
+
 w = input()
 w = int(w)
 
