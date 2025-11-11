604c7369014d3630193a52fe2d0a6ba9037aac10
Victor Pérez <perezcastillovictor@gmail.com>
2025-11-11 12:48:16 +0100

    Update 116A Tram.py
    Changed the variable name currentCapacity for capacityForStop. Not sure it's better.

--- 116A Tram.py
+++ 116A Tram.py
@@ -2,14 +2,14 @@
 #Output: the minimum capacity to accommodate all people.
 
 nStops = int(input())
-currentCapacity = 0
+capacityForStop = 0
 listCapacities = []
 
 for i in range(0, nStops):      #for every stop
     line = input()
     exits = int(line.partition(" ")[0])     #takes out of line the number of exits and enters
     enters = int(line.partition(" ")[2])
-    currentCapacity = currentCapacity + enters - exits      #caculates the capacity needed for that stop
-    listCapacities.append(currentCapacity)      #records the capacity
+    capacityForStop = capacityForStop + enters - exits      #caculates the capacity needed for that stop
+    listCapacities.append(capacityForStop)      #records the capacity
 
 print(max(listCapacities))      #returns the maximum capacity needed for the stops, which is the minimum for the train line
