c3b99009a61407746b7416f9815fb50762921c40
victorpc64 <perezcastillovictor@gmail.com>
2025-11-13 06:36:44 -0800

    Create 200B and changed 236 implementation with sets

---
+++ 200B Drinks.py
@@ -0,0 +1,14 @@
+#Input: a number and a line with a sequence of numbers separated by an empty space.
+#Output: the average of the numbers in the sequence (the sum divided by the numbers of elements)
+
+n = int(input())
+line = input()
+
+lineList = line.split(" ")      #Turns the line into a list with the numbers separated by " "
+sum = 0
+
+for i in lineList:
+    sum += int(i)       #Casts to int because they're input and stored as strings
+
+result = sum / n
+print(result)
--- 236A Boy or Girl.py
+++ 236A Boy or Girl.py
@@ -1,13 +1,8 @@
 #Input: a name.
 #Output: if the count of distinct characters is even "CHAT WITH HER"; if it's odd "IGNORE HIM!"
-#Maybe I should have implement it with a set instead of a list.
 
 name = input()
-distinctCharacters = []     #Initialize an empty list
-
-for i in name:      #Runs through all characters in name
-    if i not in distinctCharacters:     
-        distinctCharacters.append(i)    #Adds the character if it's distinct
+distinctCharacters = {c for c in name}      #Adds every distinct character in name
 
 if len(distinctCharacters) % 2 != 0:    #Checks if odd
     print("IGNORE HIM!")
