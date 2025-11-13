4307e28a3ffcfd740d231ca4bae90f94a58eb0e1
victorpc64 <perezcastillovictor@gmail.com>
2025-11-13 08:15:18 -0800

    Update 200B Drinks.py

--- 200B Drinks.py
+++ 200B Drinks.py
@@ -1,5 +1,6 @@
 #Input: a number and a line with a sequence of numbers separated by an empty space.
 #Output: the average of the numbers in the sequence (the sum divided by the numbers of elements)
+#Maybe I should change it with list comprehensions
 
 n = int(input())
 line = input()
