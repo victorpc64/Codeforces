2c81acd1411ce57950ee61f1dafbf69c270c8fbe
Victor Pérez <perezcastillovictor@gmail.com>
2026-01-23 18:51:35 +0100

    Update 116A Tram.py
    Corrected typo in comment in line 12

--- 116A Tram.py
+++ 116A Tram.py
@@ -9,7 +9,7 @@ for i in range(0, nStops):      #for every stop
     line = input()
     exits = int(line.partition(" ")[0])     #takes out of line the number of exits and enters
     enters = int(line.partition(" ")[2])
-    capacityForStop = capacityForStop + enters - exits      #caculates the capacity needed for that stop
+    capacityForStop = capacityForStop + enters - exits      #calculates the capacity needed for that stop
     listCapacities.append(capacityForStop)      #records the capacity
 
 print(max(listCapacities))      #returns the maximum capacity needed for the stops, which is the minimum for the train line
